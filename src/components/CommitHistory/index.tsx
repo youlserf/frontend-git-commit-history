34d6e6ff063f6533697917c7cb501e42889e421f
Youlserf <youlserf.cardenas@tecsup.edu.pe>
2023-11-02 20:17:41 -0500

    add url format validation

--- src/components/CommitHistory/index.tsx
+++ src/components/CommitHistory/index.tsx
@@ -5,16 +5,34 @@ import { ColumnWrapper, CustomButton, CustomInput } from "../CustomElements";
 import ItemCommitHistory from "./ItemCommitHistory";
 import "./styles.css";
 
-
 const CommitHistoryComponent: React.FC = ({ handleLogout }) => {
   const [commits, setCommits] = useState<Commit[]>([]);
-  const [owner, setOwner] = useState<string>(""); // State to capture username
-  const [repo, setRepo] = useState<string>(""); // State to capture repository name
-  const [repositoryUrl, setRepositoryUrl] = useState<string>(""); // State to capture repository URL
+  const [owner, setOwner] = useState<string>("");
+  const [repo, setRepo] = useState<string>("");
+  const [repositoryUrl, setRepositoryUrl] = useState<string>("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
+  const isValidGitHubRepoURL = (url: string) => {
+    const githubRepoPattern = /^(https?:\/\/)?(www\.)?github\.com\/([A-Za-z0-9_.-]+)\/([A-Za-z0-9_.-]+)(\.git)?$/i;
+    return githubRepoPattern.test(url);
+  };
+
+  const extractRepoNameFromURL = (urlRepo: string) => {
+    if (urlRepo.endsWith(".git")) {
+      urlRepo = urlRepo.slice(0, -4);
+    }
+    return urlRepo;
+  };
+
   const fetchCommitHistory = async () => {
+    if (!isValidGitHubRepoURL(repositoryUrl)) {
+      setError(
+        "Please enter a valid URL, example:\nhttps://github.com/youlserf/backend-git-commit-history\nhttps://github.com/youlserf/backend-git-commit-history.git"
+      );
+      return;
+    }
+
     setLoading(true);
     setError(null);
 
@@ -32,7 +50,7 @@ const CommitHistoryComponent: React.FC = ({ handleLogout }) => {
       if (repositoryUrl) {
         const urlParts = repositoryUrl.split("/");
         urlOwner = urlParts[urlParts.length - 2];
-        urlRepo = urlParts[urlParts.length - 1];
+        urlRepo = extractRepoNameFromURL(urlParts[urlParts.length - 1]);
       }
 
       const response = await instance.get(
@@ -46,7 +64,7 @@ const CommitHistoryComponent: React.FC = ({ handleLogout }) => {
     } catch (error) {
       setCommits([]);
       if (error.response && error.response.status === 404) {
-        setError("Resource not found. Please check the owner and repo.");
+        setError("Resource not found. Please check the format of the URL.");
       } else {
         setError("Error fetching commit history.");
       }
@@ -94,15 +112,15 @@ const CommitHistoryComponent: React.FC = ({ handleLogout }) => {
             </p>
           </div>
           <div className="wrapper">
-              {loading ? (
-                <div className="flex items-center justify-center h-32">
-                  <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
-                </div>
-              ) : (
-                commits.map((commit) => (
-                  <ItemCommitHistory key={commit.sha} item={commit} />
-                ))
-              )}
+            {loading ? (
+              <div className="flex items-center justify-center h-32">
+                <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+              </div>
+            ) : (
+              commits.map((commit) => (
+                <ItemCommitHistory key={commit.sha} item={commit} />
+              ))
+            )}
           </div>
         </div>
       </div>
